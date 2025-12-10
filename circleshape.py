8729642cb4b46f53b00e45da42ec6ba0536c5b2f
Alex-GHP <alex.morariu.dev@gmail.com>
2025-12-10 19:20:32 +0200

    feat: collisions

--- circleshape.py
+++ circleshape.py
@@ -19,3 +19,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        return self.radius + other.radius >= self.position.distance_to(other.position)
